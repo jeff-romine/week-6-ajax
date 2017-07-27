0cf944a1a48ca67095c629e2ddaaf48d354cc73f
Jeff Romine <jeffromine@overstock.com>
2017-07-26 23:10:21 -0600

    Comple

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,14 +20,22 @@ const TOPICS = [
 $(document).ready(
     function () {
         var topics = TOPICS.slice(0);
+        var offsets = {};
+        const QUANTITY = 12;
 
 
         function handleTopicButton(event) {
-
+            $("#img-area").empty();
             var topic = $(event.target).attr("data-topic");
+            var offset = (offsets[topic] || 0);
 
-            var queryUrl = "https://api.giphy.com/v1/gifs/search?api_key=b6c3a4726a144242a074e0ef8d296e48&q=" +
-                topic + "&limit=3&offset=0&rating=G&lang=en";
+            offsets[topic] = offset + QUANTITY;
+
+            var queryUrl = "https://api.giphy.com/v1/gifs/search?api_key=b6c3a4726a144242a074e0ef8d296e48&" +
+                "q=" + topic +
+                "&limit=" + QUANTITY +
+                "&offset=" + offset +
+                "&rating=G&lang=en";
 
             $.ajax({
                 url: queryUrl,
@@ -36,16 +44,51 @@ $(document).ready(
 
                 console.log(response);
 
-                response.data.forEach(function (giphyItem) {
-                    var imgUrl = giphyItem.images.downsized.url;
+                if (offsets[topic]) {
+                    offsets[topic] += QUANTITY;
+                }
+                else {
+                    offsets[topic] = QUANTITY;
+                }
 
+                response.data.forEach(function (giphyItem) {
+
+                    var containerDiv = $("<div>").addClass("col-md-4").addClass("thumbnail-wrapper");
+                    var thumbnailDiv = $("<div>").addClass("thumbnail");
+                    var animateUrl = giphyItem.images.downsized.url;
+                    var stillUrl = giphyItem.images.downsized_still.url;
                     var img = $("<img>")
                         .addClass("col-md-4")
-                        .attr("src",imgUrl);
+                        .addClass("gif")
+                        .attr("data-still",stillUrl)
+                        .attr("data-animate",animateUrl)
+                        .attr("data-state","still")
+                        .css("height","150px")
+                        .css("width","100%")
+                        .attr("src",stillUrl);
 
-                    $("#img-area").prepend(img);
+                    var caption = $("<div>").addClass("caption").html($("<p>").text("Rating: " + giphyItem.rating));
+
+                    thumbnailDiv.append(img);
+                    thumbnailDiv.append(caption);
+
+                    containerDiv.append(thumbnailDiv);
+                    $("#img-area").prepend(containerDiv);
                 });
 
+                $(".gif").click(function() {
+                    var img = $(this);
+                    var state = img.attr("data-state");
+
+                    if (state === "still") {
+                        img.attr("src",img.attr("data-animate"));
+                        img.attr("data-state","animate");
+                    }
+                    else {
+                        img.attr("src",img.attr("data-still"));
+                        img.attr("data-state","still");
+                    }
+                });
             });
         }
 
@@ -63,6 +106,19 @@ $(document).ready(
             $(".topic-button").click(handleTopicButton);
         }
 
+
+        $("#add-topic-submit").click(function (event) {
+
+            event.preventDefault();
+
+            var newTopic = $("#new-topic").val().toLowerCase();
+            if (newTopic && !topics.find(function (topic) {return topic === newTopic;})) {
+                topics.push(newTopic);
+                refreshButtons();
+            }
+            $("#new-topic").val("");
+            }
+        );
 
         refreshButtons();
     }
